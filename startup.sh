49bf3e24d07dbf1599cf49ecc585737ec885e3bc
thomas <tvermeersch@protonmail.com>
2020-10-09 15:19:29 +0200

    bugs

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 GVMSTATE=""
 restart_counter=0
 
-if [$(ping -c 1 google.com | grep loss | awk '{print $6}') = "0%"]
+if [$(ping -c 1 google.com | grep loss | awk '{print $6}') == "0%"]
     then 
         echo "updating the gvm vulnrability tests "
         gvm-feed-update
@@ -12,7 +12,7 @@ fi
 
 while [restart_counter <=1]
 do
-    if [$(systemctl status gvmd | grep Active | awk '{print $2}') = "inactive"]
+    if [$(systemctl status gvmd | grep Active | awk '{print $2}') == "inactive"]
         then $(gvm-start)
         restart_counter = restart_counter + 1
         GVMSTATE=$(systemctl status gvmd | grep Active | awk '{print $2}')
